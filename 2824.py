ce3f36f6e0a504b1ae0f440d5ebbb25db774ce5b
seif2001 <seifelshabshiri@aucegypt.edu>
2024-06-28 19:18:05 +0300

    2824

---
+++ 2824.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def countPairs(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        sums = []
+        sums.append(target)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                sums.append(nums[i] + nums[j])
+        sums.sort()
+        def binSearch(l, h, target):
+            if l == h:
+                if sums[l] == target:
+                    return l
+                else:
+                    return 0
+            else:
+                mid = int((l+h)/2)
+                if sums[mid] >= target:
+                    return binSearch(l, mid, target)
+                else:
+                    return binSearch(mid+1, h, target)
+
+        return binSearch(0, len(sums), target)
+        
